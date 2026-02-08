87cda6f05635234ec53b8af04e60fb6704d06be3
Ilia <infinity-ex@narod.ru>
2026-02-08 19:41:58 +0300

    MEXC prices extraction done!

--- crypto_bot.js
+++ crypto_bot.js
@@ -13,6 +13,8 @@ const version = "1.0";
 
 var pool = null;
 var exchange_data;
+var is_running = false;
+var run_id = 0;
 
 function getDateTime() {
     var dt = new Date();
@@ -42,25 +44,27 @@ async function init(){
         
     sql = "SELECT * FROM exchanges ORDER BY id";
     
-    console.log("sql", sql, args);    
+//    console.log("sql", sql, args);    
     res = await pool.query(sql, []); 
     exchange_data = {};
     for (var i = 0; i < res.rows.length; i++){
         var d = res.rows[i];
         exchange_data[d.name] = {
+            "id":       d.id,
             "key":      d.api_key,
             "secret":   d.api_secret,
             "settings": JSON.parse(d.settings)
         };
     }
         
-    console.log("exchanges", exchange_data);
+  //  console.log("exchanges", exchange_data);
 }
 
 async function test_mexc(){
     var args = null, sql, res, url;
     const request_headers = { "X-MEXC-APIKEY":  exchange_data.MEXC.key };
-    url = exchange_data.MEXC.settings.api_url + "exchangeInfo";
+    //url = exchange_data.MEXC.settings.api_url + "v3/ticker/24hr";
+    url = exchange_data.MEXC.settings.api_url + "v1/contract/ticker";
     args = {
         headers: request_headers
     };
@@ -71,17 +75,148 @@ console.log(url, args);
     
 console.log(res);    
     fs.writeFileSync("test.json", JSON.stringify(res.data));
+    process.exit(0);
+}
+
+async function save_mexc_futures_price(data){
+    const s = (""+data.symbol).trim();
+    var ar = s.split("_");
+    if ((s.indexOf("_USDT") >= 0) && (data.volume24 > 0)){
+        const sql = "INSERT INTO prices_futures (token,exchange_id,price,funding_rate,run_id) VALUES ('"
+            +(""+ar[0]).trim()+"',"
+            +exchange_data.MEXC.id+","
+            +data.lastPrice+","
+            +(data.fundingRate*100)+","
+            +run_id+")";
+        await pool.query(sql);
+    }
+}
+
+async function save_mexc_spot_price(data){
+    const s = (""+data.symbol).trim();    
+    if ((s.indexOf("USDT") >= 0) && (data.volume > 0)){
+        const sql = "INSERT INTO prices_spot (token,exchange_id,price,change_percent,run_id) VALUES ('"
+            +(""+s.replace('USDT','')).trim()+"',"
+            +exchange_data.MEXC.id+","
+            +data.lastPrice+","
+            +(data.priceChangePercent*100)+","
+            +run_id+")";
+        await pool.query(sql);
+    }
+}
+
+async function run_mexc(){
+    console.log(getDateTime(), ">run_mexc run_id = " + run_id, getDateTime());    
     
+    var args = null, sql, res, url;
+    const request_headers = { "X-MEXC-APIKEY":  exchange_data.MEXC.key };
+    
+    // get futures prices
+    url = exchange_data.MEXC.settings.api_url + "v1/contract/ticker";
+    
+    args = {
+        headers: request_headers
+    };
+    
+    res = await axios.get(url, args);
+    
+    if (res.data.success){
+        await Promise.all(
+            res.data.data.map(async (d) => {
+                await save_mexc_futures_price(d);
+            })
+        );
+    } else {
+        throw new Error("Failed to get Mexc futures prices: \n"+url);
+    }
+    
+    // get spot prices
+    
+    url = exchange_data.MEXC.settings.api_url + "v3/ticker/24hr";
+    
+    res = await axios.get(url, args);
+    
+    if (res.data){
+        await Promise.all(
+            res.data.map(async (d) => {
+                await save_mexc_spot_price(d);
+            })
+        );
+    } else {
+        throw new Error("Failed to get Mexc spot prices: \n"+url);
+    }    
+    console.log(getDateTime(), "<run_mexc run_id = " + run_id, getDateTime());    
+}
+
+async function run_gate(){
+    
+}
+
+async function run_bybit(){
+    
+}
+
+async function run(){
+    var sql;
+    is_running = true;
+    try {
+        sql = "INSERT INTO runs(progress) VALUES('starting...')";
+        await pool.query(sql);
+        
+        sql = "SELECT MAX(id) AS run_id FROM runs WHERE finished IS NULL";
+        const res = await pool.query(sql, []);
+        run_id = parseInt(res.rows[0].run_id);
+        
+        for (var key in exchange_data){
+            if (key == "MEXC") {
+                await run_mexc();
+            }
+            if (key == "GATE") {
+                await run_gate();
+            }
+            if (key == "BYBIT") {
+                await run_bybit();
+            }
+        }
+    } catch(e){
+        console.log(getDateTime(), "<crypto_bot ERROR in run! \n"+e.message, getDateTime());    
+        process.exit(0);
+    }
+    is_running = false;
+    
+    
+    process.exit();
+    
+}
+
+async function check_run(){
+    if (!is_running) {
+        const sql = "SELECT COUNT(*) AS qty FROM runs WHERE finished::time with time zone >= current_time - interval '" + process.env.period_minutes + "' minute";
+
+        console.log("sql", sql);    
+        const res = await pool.query(sql, []); 
+
+        const qty = parseInt(res.rows[0].qty);
+        if (qty <= 0){
+            await run();
+        }    
+    }
 }
 
 new Promise(async function (resolve, reject) {
     console.log(getDateTime(), ">crypto_bot main version = " + version, getDateTime());    
     await init();
     
-    await test_mexc();
+    //await test_mexc();
     
-    await pool.end();
-    process.exit(0);
+    setInterval(async function(){
+        await check_run();
+    }, 10000);
+    await check_run();
+    
+//    await pool.end();
+//    process.exit(0);
+    
 });
 
 
--- db.sql
+++ db.sql
@@ -12,49 +12,51 @@ INSERT INTO exchanges(name,api_key,api_secret) VALUES('MEXC', 'mx0vglxvo6e6R85CY
 INSERT INTO exchanges(name,api_key,api_secret) VALUES('GATE', '2f595b6d1102f9972af63e8cd4dd3b44', 'e3a17a630b968eb0b625dc8bdde3e7667993257c5e4b8779940ad04fe82d7bca');
 
 
-CREATE TABLE tokens_spot (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(20) NULL DEFAULT NULL
-);
-
-CREATE TABLE tokens_futures (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(20) NULL DEFAULT NULL
-);
-
-CREATE INDEX ON tokens_spot(name);
-CREATE INDEX ON tokens_futures(name);
-
+# DROP TABLE tokens_spot;
+DROP TABLE runs;
 CREATE TABLE runs (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT NOW()
+    id              SERIAL PRIMARY KEY,
+    progress        TEXT NULL DEFAULT NULL,
+    created         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    finished        TIMESTAMP WITH TIME ZONE
 );
+CREATE INDEX ON runs(finished);
 
-
+DROP TABLE prices_spot;
 CREATE TABLE prices_spot (
     id SERIAL PRIMARY KEY,
-    token_id INT NOT NULL,
+    token VARCHAR(20) NOT NULL,
     exchange_id INT NOT NULL,
-    price NUMERIC(10,10) NOT NULL,
+    price NUMERIC(20,10) NOT NULL,
+    change_percent NUMERIC(20,10) NOT NULL, -- 24 hours change
     run_id INT NOT NULL
 );
-CREATE INDEX ON prices_spot(token_id);
+CREATE INDEX ON prices_spot(token);
 CREATE INDEX ON prices_spot(run_id);
 CREATE INDEX ON prices_spot(exchange_id);
 CREATE INDEX ON prices_spot(price);
+CREATE INDEX ON prices_spot(change_percent);
 
+
+DROP TABLE prices_futures;
 CREATE TABLE prices_futures (
     id SERIAL PRIMARY KEY,
-    token_id INT NOT NULL,
+    token VARCHAR(20) NOT NULL,
     exchange_id INT NOT NULL,
-    price NUMERIC(10,10) NOT NULL,
-    run_id INT NOT NULL,
-    funding_rate NUMERIC(2,2) NOT NULL DEFAULT 0
+    price NUMERIC(20,10) NOT NULL,
+    funding_rate NUMERIC(20,10) NOT NULL DEFAULT 0,
+    run_id INT NOT NULL
 );
-CREATE INDEX ON prices_futures(token_id);
+CREATE INDEX ON prices_futures(token);
 CREATE INDEX ON prices_futures(run_id);
 CREATE INDEX ON prices_futures(exchange_id);
 CREATE INDEX ON prices_futures(price);
 CREATE INDEX ON prices_futures(funding_rate);
 
-UPDATE exchanges SET settings = '{"api_url":"https://api.mexc.com/api/v3/"}' WHERE id = 2;
+UPDATE exchanges SET settings = '{"api_url":"https://api.mexc.com/api/"}' WHERE id = 2;
+
+SELECT * FROM prices_futures;
+
+DELETE FROM runs;
+DELETE FROM prices_futures;
+
